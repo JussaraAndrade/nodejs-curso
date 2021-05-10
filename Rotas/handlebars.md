0a5b64e89512832d03afc4c456913b01e8baedae
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-05-09 21:55:14 -0300

    feat: handlebars

---
+++ Rotas/handlebars.md
@@ -0,0 +1,28 @@
+## handlebars
+
+É um template engine que dá muitas funcionalidades ao html.
+
+* Consegue usar a estrutura de repetição, e também estrutura condicional dentro do html.
+
+* Principalmente consegue também exibir dados que vêm do back end no arquivo html.
+
+
+```
+    https://handlebarsjs.com/
+```
+
+### Instalação:
+
+```js
+    npm install --save express-handlebars
+```
+
+Irá configurar a página html. O layout padrão vai tá dentro da pasta layouts, dentro tem um arquivo chamado `main.handlebars` simplesmente é um estrutura html:
+
+```js
+    defaultLayout: 'main'
+```
+
+### Configurar o handlebars:
+
+* Informar qual handlebar quer usar, pq existem muitas opções de template engine.
--- Rotas/index.js
+++ Rotas/index.js
@@ -1,79 +1,19 @@
-// express - é um módulo
-
 const express = require("express");
-/*
-    - Variavel app está recebendo a função express, que vem do módulo express.
-    - A função express() cria uma instancia, ou seja, uma cópia inteira do framework pra dentro da variavel app.
-    - Qualquer coisa que for usar do express, vai usar a partir da variavel app, a variavel app é a principal do sistema.  
-
-    * Para evitar erros, transforme o var em const, vai evitar que sobrescreva a variavel.
-*/
-
 const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
+//Config
+    // Template Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
 
-/* 
-    Identificar o erro: Cannot GET / : 
-
-    O erro acontece porque a nossa aplicação ainda não tem nenhuma rota definida.
-    
-    O que seria rota?
-    Rota é basicamente um caminho para aplicação. 
-
-*/ 
-
-//Função de callback/rota 1
-app.get("/", function(req, res){    
-    /* 
-        - send; serve para enviar alguma coisa.
-        - Atraves da função send dentro do objeto res que vem de resposta está enviando a mensagem. 
-    */
-
-    /*
-        - sendFile; enviar arquivos.
-        - __dirname; essa variável vai retornar diretório padrão da aplicação.
-        - Vai retornar diretório absoluto da aplicação.
-    */
-    res.sendFile(__dirname + "/html/index.html");
-});
-
-//Função de callback/rota 2
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html");
+// Conexão com banco de dados Mysql
+const sequelize = new Sequelize('test', 'root', 'root', {
+    host: "localhost",
+    dialect: 'mysql'
 })
 
-//Função de callback/rota 3
-app.get("/blog", function(req, res){
-    res.send("Bem-vindo ao meu blog!")
-})
-
-/*
- Rotas com parâmetros:
- - A partir dos dois pontos, criar os parametros.
-
- - Req; é a primeira variável que cria na função de callback.
- Ela é responsavel por receber todos dados de um requisição.
-
- - Atraves do objeto req consegue obter dados da requisição 
- que foi feita, inclusive o parâmetro.
-
- Informações importantes:
-
- - Você só pode chamar função send() em uma única vez, 
-   dentro de uma função de uma rotas.
-*/
-app.get('/ola/:cargo/:nome/:cor', function(req, res){
-    res.send("<h1>Ola " +req.params.nome+ "</h1>"
-            + "<h2> Seu cargo e: " +req.params.cargo+ "</h2>"
-            + "<h3> Sua cor favorite e: " +req.params.cor+ "</h2>");
- 
-})
-
-
-/* 
-    - Essa função app.listen é executada, ela fala para o node que a aplicação já está rodando.
-    - O node vai lá e executa a função em seguida. 
-*/
 // última linha do código
 app.listen(8081, function(){
     console.log("Servidor Rodando!");
