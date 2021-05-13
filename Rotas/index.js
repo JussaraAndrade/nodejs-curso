4365b002915d909074ded36f4250137ebfa5ed00
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-05-13 12:57:52 -0300

    feat: enviando dados do formulário

--- Rotas/index.js
+++ Rotas/index.js
@@ -19,6 +19,10 @@ app.get('/cad', function(req, res){
     res.render('formulario')
 })
 
+app.post('/add', function(req, res){
+    res.send('FORMULÁRIO RECEBIDO!')
+})
+
 // última linha do código
 app.listen(8081, function(){
     console.log("Servidor Rodando!");
