3586b083109050be959ed633ab5490684538db9d
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-05-13 15:40:14 -0300

    feat: deletando posts

--- Rotas/index.js
+++ Rotas/index.js
@@ -53,7 +53,13 @@ app.post('/add', function(req, res){
     })
 })
 
-
+app.get('/deletar/:id', function(req, res){
+    Post.destroy({where: {'id': req.params.id}}).then(function(){
+        res.send("Postagem deletada com sucesso!")
+    }).catch(function(erro){
+        res.send("Esta postagem não existe!")
+    })
+})
 
 // última linha do código
 app.listen(8081, function(){
