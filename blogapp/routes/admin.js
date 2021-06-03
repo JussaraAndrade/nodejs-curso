e1769d75e91908cef9829ae043a5fb2bdd7058cc
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-03 20:28:16 -0300

    feat: formulário de postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -120,6 +120,19 @@ router.post("/categorias/deletar", (req, res) => {
     })
 })
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário")
+        res.redirect("/admin")
+    })
+})
+
 
 
 // No final do arquivo precisa exportar o router
