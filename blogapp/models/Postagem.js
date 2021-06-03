8f7f697c59e943c792fe4ba215b3228259883aa8
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-03 19:48:25 -0300

    feat: definindo model de postagens

---
+++ blogapp/models/Postagem.js
@@ -0,0 +1,34 @@
+const mongosse = require("mongoose")
+const Schema = mongosse.Schema;
+
+const Postagem = new Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    conteudo: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        // Referencia entre dois documentos Postagem/Categoria
+        // Categoria vai armazenar um id
+        type: Schema.Types.ObjectId,
+        ref: "categorias",
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+mongosse.model("postagens", Postagem)
