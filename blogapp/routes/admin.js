9499229f3d6365458003b2e0ddb8f0a5ed1458df
Jussara Andrade <jussara.jandrade1993@outlook.com>
2021-06-07 10:18:58 -0300

    feat: editando postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -170,6 +170,51 @@ router.post("/postagens/nova", (req, res) => {
     }
 })
 
+router.get("/postagens/edit/:id", (req, res) => {
+    // Primeiro pesquisando uma postagem
+    Postagem.findById({_id: req.params.id}).lean().then((postagem) => {
+        // Depois pesquisando por uma categoria
+        Categoria.find().lean().then((categorias) => {
+            // Depois está renderizado os dados na views
+            res.render("admin/editpostagens", {categorias: categorias, postagem: postagem})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/admin/postagens")
+        })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário de edição")
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.post("/postagens/edit", (req, res) => {
+
+    Postagem.findById({_id: req.body.id}).sort({data: 'desc'}).then((postagem) => {
+
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(() => {
+            req.flash("success_msg", "Postagem editada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            // Descobrir o erro
+            console.log(err)
+
+            req.flash("error_msg", "Erro interno")
+            res.redirect("/admin/postagens")
+        })
+
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao salvar a edição")
+        res.redirect("/admin/postagens")
+    })
+})
+
 
 
 // No final do arquivo precisa exportar o router
